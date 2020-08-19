cda49667be041ff33e5e0dcd8351de8fa5baf533
Maximiliano Sussini <maxsus60@gmail.com>
2020-08-19 15:49:03 +0200

    Fixed the issue with the DB seeder dropping tables, creating conflicts with the products ID in the shopping cart

--- server/bin/seeds/seeder.js
+++ server/bin/seeds/seeder.js
@@ -12,49 +12,91 @@ const dataUsers = require("./data/users");
 const config = require("../../config");
 const DB_LOCAL = config.MONGODB_URL;
 
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(DB_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.log("Error connecting to the DB", error);
+  }
+};
+
+const dropCollection = async (collection) => {
+  const collectionName = collection.collection.collectionName;
+  try {
+    console.log(`Removing all documents from "${collectionName}" collection`);
+    await collection.deleteMany();
+  } catch (error) {
+    console.log(
+      `Error when trying to delete the collection "${collectionName}"`,
+      error
+    );
+  }
+};
+
+const checkCollectionExist = async (collection, data, dataPropForErrorInfo) => {
+  try {
+    const collectionName = collection.collection.collectionName;
+    const isCollectionExists = await collection.find({});
+
+    if (!isCollectionExists.length) {
+      console.log(
+        `Collection "${collection.collection.collectionName}" does not exist`
+      );
+
+      populateCollection(collection, data, dataPropForErrorInfo);
+    } else {
+      console.log(`Collection "${collectionName}" exist`);
+    }
+  } catch (error) {
+    const collectionName = collection.collection.collectionName;
+    console.log(
+      `Error checking if the collection "${collectionName}" exists`,
+      error
+    );
+  }
+};
+
+const populateCollection = (collection, data, dataPropForErrorInfo) => {
+  const collectionName = collection.collection.collectionName;
+  const promises = [];
+  try {
+    data.forEach((item) => {
+      const promiseToResolve = collection.create(item);
+      promises.push(promiseToResolve);
+    });
+
+    console.log(`Collection "${collectionName}" successfully seeded!`);
+    return Promise.all(promises);
+  } catch (error) {
+    console.log(
+      "Error while populating DB: item =>",
+      item[dataPropForErrorInfo]
+    );
+  }
+};
+
 module.exports = {
-  syncData: () => {
-    return mongoose
-      .connect(DB_LOCAL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      })
-      .then(() => {
-        return Product.deleteMany();
-      })
-      .then(() => {
-        const promises = [];
-        dataProducts.products.forEach((item) => {
-          const promiseToResolve = Product.create(item).catch((err) =>
-            console.log(
-              "Error while populating DB: item with title",
-              item.title
-            )
-          );
-          promises.push(promiseToResolve);
-        });
+  syncData: async () => {
+    try {
+      const connection = await connectToDB();
 
-        console.log("Products synchronized successfully!");
-        return Promise.all(promises);
-      })
-      .then(() => {
-        return User.deleteMany();
-      })
-      .then(() => {
-        const promises = [];
-        dataUsers.users.forEach((item) => {
-          const promiseToResolve = User.create(item).catch((err) =>
-            console.log("Error while populating DB: item with name", item.name)
-          );
-          promises.push(promiseToResolve);
-        });
+      const checkProductCollectionExists = checkCollectionExist(
+        Product,
+        dataProducts.products,
+        "title"
+      );
 
-        console.log("Users synchronized successfully!");
-        return Promise.all(promises);
-      })
-      .catch((err) => {
-        console.log("Error connecting to the DB", err);
-      });
+      const checkUserCollectionExists = checkCollectionExist(
+        User,
+        dataUsers.users,
+        "name"
+      );
+    } catch (error) {
+      console.log("Oop... Something went wrong!", error);
+    }
   },
 };
